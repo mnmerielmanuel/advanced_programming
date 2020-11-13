c9cb88c4f29e4bcacb8b994b78e3726b266c154c
Meriel Necole T. Manuel <56289574+mnmerielmanuel@users.noreply.github.com>
2020-11-13 18:44:34 +0800

    adding assign_two

--- index.php
+++ index.php
@@ -1,4 +1,55 @@
 <?php
-echo "Hello World";
+<?php
+class User
+{
+  public $name;
+  public $username;
+  public $email;
+
+
+  function set_name($name)
+  {
+      $this->name=$name;
+  }
+  function get_name()
+  {
+      return $this->name;
+  }
+  function set_username($username)
+  {
+      $this->username=$username;
+  }
+  function get_username()
+  {
+      return $this-> username;
+  }
+  function set_email($email)
+  {
+      $this->email=$email;
+  }
+  function get_email()
+  {
+      return $this->email;
+  }
+  function addFriend($name)
+  {
+  echo $name . " added a friend <br>";
+    }
+  function postStatus($name)
+  {
+  echo $name . " posted a status <br>";
+    }
+}
+$example = new User();
+
+$example->set_name("Meriel Necole T. Manuel");
+$example->set_email("mnmerielmanuel@gmail.com");
+$example->set_username("Riel");
+echo "Name: " . $example->get_name() . "<br>";
+echo "Username: " . $example->get_username() . "<br>";
+echo "Email: " . $example->get_email() . "<br>";
+$example->addFriend("You");
+$example->postStatus("Meriel");
+
 
 ?>
